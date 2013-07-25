87075adee9d2e12ed9929071a205ca05c3f1e0c4
Jeff Principe <princjef@gmail.com>
2013-07-25 13:25:51 -0400

    Created actual node module

--- makeErd.sh
+++ makeErd.sh
@@ -9,6 +9,6 @@ done
 DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 
 mkdir -p build
-rm -f ${DIR}/build/erd.gv && node ${DIR}/js/gvCreator.js
+rm -f ${DIR}/build/erd.gv && node bin/generate --outDir ${DIR}/build/ --yamlDir ${DIR}/yaml/
 rm -f ${DIR}/build/erd.png && dot ${DIR}/build/erd.gv | neato -n -Tpng -o${DIR}/build/erd.png
 
